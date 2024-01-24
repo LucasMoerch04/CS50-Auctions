2a980affebaea78c0797eff64fdb6444a20ab7d9
LucasMoerch04 <lucasmoerch04@icloud.com>
2024-01-24 19:12:56 +0100

    add listings to index

--- Downloads/commerce/commerce/auctions/models.py
+++ Downloads/commerce/commerce/auctions/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Bids(models.Model):
+    bid = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='userBid')
+    
+class Listing(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=600)
+    price = models.ForeignKey(Bids, on_delete=models.CASCADE, blank=True, null=True)
+    
